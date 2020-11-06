58dc258f225304d1417a0deb15c394a0f306505a
ToastCoder <vicky.pcbasic@gmail.com>
2020-11-06 23:31:03 +0530

    Added data preprocessing

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# CUSTOMER SEGMENTATION USING KNN
 
+# IMPORTING REQUIRED MODULES
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+# DATA PREPROCESSING
+data = pd.read_csv("data/CustData.csv")
+x = data.iloc[:,[2,3]].values
+y = data.iloc[:,-1].values
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
