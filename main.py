4648f3c5f56e219d9e1cd3c15785fa3b11c59e05
Vigneshwar Ravichandar <vicky.pcbasic@gmail.com>
2020-11-11 12:44:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# 0 - Young adult
-# 1 - Engineering Student
-# 2 - Medical Student
-# 3 - Teachers
-# 4 - Adults
-# 5 - Travelling kinda person
-# 6 - Media Freak
-
 # IMPORTING REQUIRED MODULES
 import numpy as np
 import pandas as pd
