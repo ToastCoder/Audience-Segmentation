c895d2d1dcec256b1fce53da5e9244b0bb36c731
ToastCoder <vicky.pcbasic@gmail.com>
2020-11-11 13:05:37 +0530

    Added required modules

--- visualize.py
+++ visualize.py
@@ -1,1 +1,12 @@
+#  CUSTOMER SEGMENTATION USING LOGISTIC REGRESSION
 
+# DEVELOPED BY:
+# VIGNESHWAR RAVICHANDAR
+# MOULISHANKAR M R 
+
+# IMPORTING REQUIRED MODULES
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+import pickle
