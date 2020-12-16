a8094c7dfbded5e1a68f19cb29f2cde347a3a0a4
ToastCoder <vicky.pcbasic@gmail.com>
2020-12-17 00:45:48 +0530

    FInal conversion

--- train.py
+++ train.py
@@ -8,17 +8,44 @@
 # IMPORTING REQUIRED MODULES
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-import pickle
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import save_model
 
-# DATA PREPROCESSING
+# DATA SEGMENTATION
 data = pd.read_csv("data/custData2.csv")
 x = data.iloc[:,[4,5,6,7,8,9,10]].values
-y = data.iloc[:,-1].values
+y = data.iloc[:,11].values
 
-# INITIALIZING AND TRAINING THE MODEL WITH THE SEGMENTED DATA
-log = LogisticRegression(random_state = 0)
-log.fit(x,y)
+# ENCODING LABELS
+encoder = LabelEncoder()
+encoder.fit(y)
+y = encoder.transform(y)
+y = to_categorical(y)
 
-# SAVING THE MODEL TO A FILE
-pickle.dump(log,open("model/model.pkl","wb"))
+# SPLITTING THE MAIN DATA INTO TRAINING AND VALIDATION DATA
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size = 0.1, random_state = 0)
+
+# DEFINING THE NEURAL NETWORK LAYERS AND THEIR NODES
+model = Sequential()
+model.add(Dense(14, input_dim = 7 , activation = 'relu'))
+model.add(Dense(16, activation = 'relu'))
+model.add(Dense(16, activation = 'relu'))
+model.add(Dense(16, activation = 'relu'))
+model.add(Dense(7, activation = 'softmax'))
+
+# TRAINING THE MODEL
+model.compile(loss = 'categorical_crossentropy' , optimizer = 'adam' , metrics = ['accuracy'] )
+model.fit(x_train, y_train, epochs = 20, batch_size = 2)
+
+# CALCULATING THE ESTIMATED ACCURACY
+score = model.evaluate(x_val, y_val)
+print(f"The estimated accuracy of the model is: {round(score[1]*100,4)}")
+
+# SAVING THE MODEL
+save_model(model,'./model/custDataModel')
