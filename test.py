6cbc0e8a8be1358ae078776d8d3fb54c23f54086
ToastCoder <vicky.pcbasic@gmail.com>
2020-12-17 11:44:16 +0530

    Rebuilt with tf

--- test.py
+++ test.py
@@ -8,28 +8,34 @@
 # IMPORTING REQUIRED MODULES
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-import pickle
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import to_categorical
 
 # DATA PREPROCESSING
 data = pd.read_csv("data/custData2.csv")
 x = data.iloc[:,[4,5,6,7,8,9,10]].values
-y = data.iloc[:,-1].values
+y = data.iloc[:,11].values
 
-# OPENING AND DEFINING THE TRAINED MODEL
-log = pickle.load(open("model/model.pkl", 'rb'))
+# ENCODING LABELS
+encoder = LabelEncoder()
+encoder.fit(y)
+y = encoder.transform(y)
+y = to_categorical(y)
+
+# OPENING THE TRAINED MODEL
+model = load_model('./model/custDataModel')
 
 # GETTING AND PROCESSING DATA FROM USER
 interests = ["Technology","Politics","Food","Education","Media","Travel","Medicine"]
 user_interests = []
 for i in range(len(interests)):
-    ch = input("Is the person interested in viewing content related to "+interests[i]+"? (Y/N): ").upper()
-    if ch == "Y" or "YES":
-        user_interests.append(1)
-    elif ch == "N" or "NO":
-        user_interests.append(0)
+    val = int(input("Amount of videos person watched which is related to "+interests[i]+": "))
+    user_interests.append(val)
 
 categories = ["Young Adult","Engineering Student","Medical Student","Teacher","Mature (40+)","Travelophilic","Media Addicted"]
-res = log.predict([user_interests])
+res = model.predict([user_interests])
+res = np.argmax(res, axis=None, out=None)
 print("The person may be a "+categories[int(res)]+" type of audience.")
 
