ea0195cf2e970cfe8e9d74ed339180aedf7db9b7
ToastCoder <vicky.pcbasic@gmail.com>
2020-12-22 22:44:07 +0530

    Optimized code

--- train.py
+++ train.py
@@ -9,22 +9,14 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import save_model
 
 # DATA SEGMENTATION
 data = pd.read_csv("data/custData.csv")
 x = data.iloc[:,[4,5,6,7,8,9,10]].values
 y = data.iloc[:,11].values
-
-# ENCODING LABELS
-encoder = LabelEncoder()
-encoder.fit(y)
-y = encoder.transform(y)
-y = to_categorical(y)
 
 # SPLITTING THE MAIN DATA INTO TRAINING AND VALIDATION DATA
 x_train, x_val, y_train, y_val = train_test_split(x, y, test_size = 0.1, random_state = 0)
@@ -38,7 +30,7 @@ model.add(Dense(16, activation = 'relu'))
 model.add(Dense(7, activation = 'softmax'))
 
 # TRAINING THE MODEL
-model.compile(loss = 'categorical_crossentropy' , optimizer = 'adam' , metrics = ['accuracy'] )
+model.compile(loss = 'sparse_categorical_crossentropy' , optimizer = 'adam' , metrics = ['accuracy'] )
 model.fit(x_train, y_train, epochs = 20, batch_size = 2)
 
 # CALCULATING THE ESTIMATED ACCURACY
