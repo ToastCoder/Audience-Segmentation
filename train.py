a0de7c96926e524162fe5a6dd1c578ad61df1ba9
ToastCoder <vicky.pcbasic@gmail.com>
2021-01-02 11:54:02 +0530

    Several changes

--- train.py
+++ train.py
@@ -1,41 +1,65 @@
-#  AUDIENCE SEGMENTATION
+# AUDIENCE SEGMENTATION
+
 # FILE NAME: train.py
 
-# DEVELOPED BY:
-# VIGNESHWAR RAVICHANDAR
-# MOULISHANKAR M R 
+# DEVELOPED BY: Vigneshwar Ravichandar
+
+# TOPICS: Multiclass Classification, Machine Learning, TensorFlow
+
+# DISABLE TENSORFLOW DEBUG INFORMATION
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+print("TensorFlow Debugging Information is hidden.")
 
 # IMPORTING REQUIRED MODULES
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import save_model
+import tensorflow as tf
+
+print(f"TensorFlow version: {tf.__version__}")
+
+DATASET_PATH = 'data/custData.csv'
+MODEL_PATH = './model/custDataModel'
+ACC_THRESHOLD = 0.99
 
 # DATA SEGMENTATION
-data = pd.read_csv("data/custData.csv")
+data = pd.read_csv(DATASET_PATH)
+print(data.describe())
+
 x = data.iloc[:,[4,5,6,7,8,9,10]].values
 y = data.iloc[:,11].values
+
 
 # SPLITTING THE MAIN DATA INTO TRAINING AND VALIDATION DATA
 x_train, x_val, y_train, y_val = train_test_split(x, y, test_size = 0.1, random_state = 0)
 
-# DEFINING THE NEURAL NETWORK LAYERS AND THEIR NODES
-model = Sequential()
-model.add(Dense(14, input_dim = 7 , activation = 'relu'))
-model.add(Dense(16, activation = 'relu'))
-model.add(Dense(16, activation = 'relu'))
-model.add(Dense(16, activation = 'relu'))
-model.add(Dense(7, activation = 'softmax'))
+# DEFINING THE NEURAL NETWORK FUNCTION
+def custDataModel():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Dense(14, input_dim = 7 , activation = 'relu'))
+    model.add(tf.keras.layers.Dense(16, activation = 'relu'))
+    model.add(tf.keras.layers.Dense(16, activation = 'relu'))
+    model.add(tf.keras.layers.Dense(16, activation = 'relu'))
+    model.add(tf.keras.layers.Dense(7, activation = 'softmax'))
+    return model
+
+class Callback(tf.keras.callbacks.Callback): 
+    def on_epoch_end(self, epoch, logs={}): 
+        if(logs.get('accuracy') > ACC_THRESHOLD):   
+            print("Reached Threshold Accuracy, Stopping Training.")   
+            self.model.stop_training = True
+
+model = custDataModel()
+callback = Callback()
 
 # TRAINING THE MODEL
 model.compile(loss = 'sparse_categorical_crossentropy' , optimizer = 'adam' , metrics = ['accuracy'] )
-model.fit(x_train, y_train, epochs = 20, batch_size = 2)
+model.fit(x_train, y_train, epochs = 20, batch_size = 2,callbacks = [callback])
 
 # CALCULATING THE ESTIMATED ACCURACY
 score = model.evaluate(x_val, y_val)
-print(f"The estimated accuracy of the model is: {round(score[1]*100,4)}")
+print(f"Model Accuracy: {round(score[1]*100,4)}")
 
 # SAVING THE MODEL
-save_model(model,'./model/custDataModel')
+tf.keras.models.save_model(model,'./model/custDataModel')
