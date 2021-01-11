a9193c8953865a6e2a84eb8aa609eee6d82944f2
ToastCoder <vicky.pcbasic@gmail.com>
2021-01-11 20:03:41 +0530

    Some changes

--- src/test.py
+++ src/test.py
@@ -6,11 +6,6 @@
 
 # TOPICS: Multiclass Classification, Machine Learning, TensorFlow
 
-# DISABLE TENSORFLOW DEBUG INFORMATION
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-print("TensorFlow Debugging Information is hidden.")
-
 # IMPORTING REQUIRED MODULES
 import numpy as np
 import pandas as pd
