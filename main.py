d780db89b46f5744b319dc0d069fa94e2d67438a
ToastCoder <vicky.pcbasic@gmail.com>
2021-01-11 19:39:51 +0530

    Some changes

--- main.py
+++ main.py
@@ -30,8 +30,21 @@ def parse():
     parser = argparse.ArgumentParser(description = 'Command Line Interface for Audience Segmentation')
     parser.add_argument('-tr','--train', type = strBool, help = 'Argument taken for training model.', default = "False")
     parser.add_argument('-req','--install_requirements', type = strBool, help = 'Argument taken for installing requirements', default = False)
-    parser.add_argument('-v','--visualize', type = strBool, help = 'Argument taken for visualizing metrics', default = False)
     parser.add_argument('-t','--test', type = strBool, help = 'Argument for testing with custom input',required = True)
     args = parser.parse_args()
     return args
 
+# MAIN FUNCTION
+if __name__ == "__main__":
+
+    args = parse()
+
+    if (args.install_requirements):
+        os.system('sudo apt install python3-pip')
+        os.system('pip3 install -r requirements.txt')
+    
+    if (args.train):
+        os.system('python3 src/train.py')
+
+    if (args.test):
+        os.system('python3 src/test.py')
