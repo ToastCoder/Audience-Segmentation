67042df07a2d97a4e4a9c651621d39fb584ed48f
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-20 12:52:54 +0530

    Some changes

--- src/visualize.py
+++ src/visualize.py
@@ -1,3 +1,5 @@
+#-------------------------------------------------------------------------------------------------------------------------------
+
 # AUDIENCE SEGMENTATION
 
 # FILE NAME: visualize.py
@@ -5,6 +7,10 @@
 # DEVELOPED BY: Vigneshwar Ravichandar, Moulishankar M R
 
 # TOPICS: Multiclass Classification, Machine Learning, TensorFlow
+
+#-------------------------------------------------------------------------------------------------------------------------------
+
+# IMPORTING REQUIRED MODULES
 import os
 import numpy as np
 import pandas as pd
@@ -14,14 +20,13 @@ import matplotlib.pyplot as plt
 
 print(f"TensorFlow version: {tf.__version__}")
 
-os.system('cd ..')
-
 DATASET_PATH = 'data/custData.csv'
 MODEL_PATH = './model/custDataModel'
 
 # DATA SEGMENTATION
 data = pd.read_csv(DATASET_PATH)
-print(data.describe())
+print("Dataset Description:\n",data.describe())
+print("Dataset Head\n",data.head())
 
 x = data.iloc[:,[4,5,6,7,8,9,10]].values
 y = data.iloc[:,11].values
