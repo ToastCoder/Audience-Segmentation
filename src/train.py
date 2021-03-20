6ce9294c7318a58da8b5c2c2a4e2570fc4ecfce0
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-20 12:47:45 +0530

    Some changes

--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+#-------------------------------------------------------------------------------------------------------------------------------
+
 # AUDIENCE SEGMENTATION
 
 # FILE NAME: train.py
@@ -6,6 +8,8 @@
 
 # TOPICS: Multiclass Classification, Machine Learning, TensorFlow
 
+#-------------------------------------------------------------------------------------------------------------------------------
+
 # IMPORTING REQUIRED MODULES
 import os
 import numpy as np
@@ -13,19 +17,47 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import argparse
+
+# FUNCTION FOR PARSING ARGUMENTS
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e','--epochs',
+                        type = int, 
+                        default = 50, 
+                        required = False)
+
+    parser.add_argument('-bs', '--batch_size',
+                        type = int, 
+                        default = 5, 
+                        required = False)
+
+    parser.add_argument('-l','--loss',
+                        type = str, 
+                        default = 'sparse_categorical_crossentropy', 
+                        required = False)
+
+    parser.add_argument('-op','--optimizer', 
+                        type = str, 
+                        default = 'adam', 
+                        required = False)
+
+    args = parser.parse_args()
+    return args
 
 print(f"TensorFlow version: {tf.__version__}")
 
 DATASET_PATH = 'data/custData.csv'
 MODEL_PATH = './model/custDataModel'
+args = parse()
 
 # DATA SEGMENTATION
 data = pd.read_csv(DATASET_PATH)
-print(data.describe())
+print("Dataset Description:\n",data.describe())
+print("Dataset Head\n",data.head())
 
 x = data.iloc[:,[4,5,6,7,8,9,10]].values
 y = data.iloc[:,11].values
-
 
 # SPLITTING THE MAIN DATA INTO TRAINING AND VALIDATION DATA
 x_train, x_val, y_train, y_val = train_test_split(x, y, test_size = 0.1, random_state = 0)
@@ -40,14 +72,14 @@ def custDataModel():
     model.add(tf.keras.layers.Dense(7, activation = 'softmax'))
     return model
 
-
+# INITITIALIZING THE CALLBACK
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor = 'accuracy', mode = 'max')
 
 model = custDataModel()
 
-
 # TRAINING THE MODEL
-model.compile(loss = 'sparse_categorical_crossentropy' , optimizer = 'adam' , metrics = ['accuracy'] )
-history = model.fit(x_train, y_train, epochs = 50, batch_size = 5, callbacks = [callback], validation_data = (x_val, y_val))
+model.compile(loss = args.loss , optimizer = args.optimizer , metrics = ['accuracy'] )
+history = model.fit(x_train, y_train, epochs = args.epochs, batch_size = args.batch_size, callbacks = early_stopping, validation_data = (x_val, y_val))
 
 # PLOTTING THE GRAPH FOR TRAIN-LOSS AND VALIDATION-LOSS
 plt.figure(0)
